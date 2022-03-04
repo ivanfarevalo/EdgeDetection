d45fb5bca5531c5fbaecbfd97b3cc96d60c36e54
Ivan Arevalo <ivan.felipe.ag@gmail.com>
2022-03-04 11:35:16 -0800

    Stable v1.1.0: Tested on 9 input 9 output module of various types

--- src/BQ_run_module.py
+++ src/BQ_run_module.py
@@ -12,13 +12,25 @@ def run_module(input_path_dict, output_folder_path, min_hysteresis=100, max_hyst
 
     # Get input file paths from dictionary
     input_img_path = input_path_dict['Input Image']
+    input_img_path2 = input_path_dict['Input Image2']
+    input_img_path3 = input_path_dict['Input Image3']
     input_csv_path = input_path_dict['Input CSV']
+    input_csv_path2 = input_path_dict['Input CSV2']
+    input_csv_path3 = input_path_dict['Input CSV3']
     input_npy_path = input_path_dict['Input Npy']
+    input_npy_path2 = input_path_dict['Input Npy2']
+    input_npy_path3 = input_path_dict['Input Npy3']
 
     # Read in data from each path
     img = cv2.imread(input_img_path, 0)
+    img2 = cv2.imread(input_img_path2, 0)
+    img3 = cv2.imread(input_img_path3, 0)
     csv = pd.read_csv(input_csv_path)
+    csv2 = pd.read_csv(input_csv_path2)
+    csv3 = pd.read_csv(input_csv_path3)
     npy = np.load(input_npy_path)
+    npy2 = np.load(input_npy_path2)
+    npy3 = np.load(input_npy_path3)
 
     
     ##### Run algorithm #####
@@ -31,24 +43,48 @@ def run_module(input_path_dict, output_folder_path, min_hysteresis=100, max_hyst
     
     # Get filename
     input_img_name = os.path.split(input_img_path)[-1][:-4]
+    input_img_name2 = os.path.split(input_img_path2)[-1][:-4]
+    input_img_name3 = os.path.split(input_img_path3)[-1][:-4]
     input_csv_name = os.path.split(input_csv_path)[-1][:-4]
+    input_csv_name2 = os.path.split(input_csv_path2)[-1][:-4]
+    input_csv_name3 = os.path.split(input_csv_path3)[-1][:-4]
     input_npy_name = os.path.split(input_npy_path)[-1][:-4]
+    input_npy_name2 = os.path.split(input_npy_path2)[-1][:-4]
+    input_npy_name3 = os.path.split(input_npy_path3)[-1][:-4]
     
     # Generate desired output file names and paths
     output_img_path = "%s/%s_out.jpg" % (output_folder_path, input_img_name)
+    output_img_path2 = "%s/%s_out.jpg" % (output_folder_path, input_img_name2)
+    output_img_path3 = "%s/%s_out.jpg" % (output_folder_path, input_img_name3)
     output_csv_path = "%s/%s_out.csv" % (output_folder_path, input_csv_name)
+    output_csv_path2 = "%s/%s_out.csv" % (output_folder_path, input_csv_name2)
+    output_csv_path3 = "%s/%s_out.csv" % (output_folder_path, input_csv_name3)
     output_npy_path = "%s/%s_out.npy" % (output_folder_path, input_npy_name)
+    output_npy_path2 = "%s/%s_out.npy" % (output_folder_path, input_npy_name2)
+    output_npy_path3 = "%s/%s_out.npy" % (output_folder_path, input_npy_name3)
 
     # Save output files
     cv2.imwrite(output_img_path, img)
+    cv2.imwrite(output_img_path2, img2)
+    cv2.imwrite(output_img_path3, img3)
     csv.to_csv(output_csv_path)
+    csv2.to_csv(output_csv_path2)
+    csv3.to_csv(output_csv_path3)
     np.save(output_npy_path, npy)
+    np.save(output_npy_path2, npy2)
+    np.save(output_npy_path3, npy3)
 
     # Create dictionary of output paths
     output_paths_dict = {}
     output_paths_dict['Image Out'] = output_img_path
+    output_paths_dict['Image Out2'] = output_img_path2
+    output_paths_dict['Image Out3'] = output_img_path3
     output_paths_dict['CSV Out'] = output_csv_path
+    output_paths_dict['CSV Out2'] = output_csv_path2
+    output_paths_dict['CSV Out3'] = output_csv_path3
     output_paths_dict['Npy Out'] = output_npy_path
+    output_paths_dict['Npy Out2'] = output_npy_path2
+    output_paths_dict['Npy Out3'] = output_npy_path3
     
     ##### Return output file path #####  -> Important step
     return output_paths_dict
